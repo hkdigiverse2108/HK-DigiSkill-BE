04a862de093e74d697ebf8d7accc23b0cbd41cf0
hkdigiverse2108 <hkdigiversework@gmail.com>
2026-03-03 15:00:42 +0530

    feat: Add workshop routes for managing and accessing workshop data.

--- src/Routes/workshop.ts
+++ src/Routes/workshop.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/add', adminJWT, workshopController.add_workshop);
 router.post('/edit', adminJWT, workshopController.edit_workshop_by_id);
 router.delete('/delete/:id', adminJWT, workshopController.delete_workshop_by_id);
-router.get('/all', adminJWT, workshopController.get_all_workshop);
+router.get('/all', userJWT, workshopController.get_all_workshop);
 router.get('/my-workshops', userJWT, workshopController.get_my_workshops);
 router.post('/purchase', adminJWT, workshopController.purchase_workshop);
 router.get('/:id', userJWT, workshopController.get_workshop_by_id);
